efa10e3949eaad0e5e32fb564c7238f93b7bca67
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2023-01-17 21:54:22 -0300

    Instala ORM

--- app/server.py
+++ app/server.py
@@ -1,23 +1,28 @@
 from flask import Flask,jsonify,request 
 from dotenv import load_dotenv
 import os
+from flask_sqlalchemy import SQLAlchemy
 
 
 load_dotenv()
-
-api = Flask(__name__)
 
 #Variaveis de ambiente
 PORT = os.environ["PORT"]
 HOST = os.environ["HOST"]
 DEBUG = os.environ["DEBUG"]
+URL_BASE = os.environ["URL_BASE"]
+SQL_PORT = os.environ["SQL_PORT"]
+DB_NAME = os.environ["DB_NAME"]
 
-print(PORT,HOST,DEBUG)
+api = Flask(__name__)
+api.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{URL_BASE}:{SQL_PORT}/{DB_NAME}'
+db = SQLAlchemy(api)
 
+
+#Rotas 
 @api.route('/')
 def obter_itens():
     return jsonify({'id': 1 ,'nome' : 'machado'})
 
 
 api.run(debug=DEBUG,host=HOST,port=PORT)
-
